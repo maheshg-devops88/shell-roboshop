e60252d5e964f6c4c0d8ce713134a6e01ce357db
mahesh <mahesh9.storage@gmail.com>
2026-07-23 09:53:54 +0530

    roboshop shell scripts

--- 01.ec2_creation.sh
+++ 01.ec2_creation.sh
@@ -5,11 +5,28 @@ Instance_Type="t3.micro"
 SG_ID="sg-020b6d9853c171a73"
 SUB_ID="subnet-0a76f340dca5f78a5" 
 
+for instance in $@; do
 
-aws ec2 run-instances \
-    --image-id $AMI_ID \
+aws ec2 run-instances --image-id $AMI_ID \
     --instance-type $Instance_Type \
     --security-group-ids $SG_ID \
     --subnet-id $SUB_ID \
-    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" \
-    --count 1
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" --count 1
+
+done
+
+
+
+# aws ec2 describe-instances \
+#     --instance-ids i-0123456789abcdef0 \
+#     --query "Reservations[*].Instances[*].PublicIpAddress" \
+#     --output text
+
+# for Name in $@
+   
+#     if [ $Name==frontend ]; then
+       
+#     public_ip="aws ec2 describe-instances --instance-ids i-0123456789abcdef0 \
+#             --query "Reservations[*].Instances[*].PublicIpAddress" --output text"
+   
+   
