27ed688e92e6491080b1eefe39c00d3510617cfe
mahesh <mahesh9.storage@gmail.com>
2026-07-24 14:42:43 +0530

    added mongodb script

---
+++ 02.mongodb.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+userid=$(id -u)
+
+if [ $userid -ne 0 ]; then
+    
+    echo "Please run the script sudo access"
+    exit 1
+fi 
+
+VALIDATE()
+if [$1 == 0 ]; then
+   echo "$2 is SuccessFul" 
+ else
+   echo "$2 is Failure"
+fi
+
+cp mongo.repo /etc/yum.repos.d/
+VALIDATE $? "Copy Process"
+
+dnf install mongodb-org -y
+VALIDATE $? "mongodb Installation"
+
+systemctl enable mongod 
+VALIDATE $? "mongodb enabled"
+
+
+sed -i 's/127.0.0.1/0.0.0.0/g'  /etc/mongod.conf
+VALIDATE $? "Changing to 127.0.0.1 to 0.0.0.0" 
+
+systemctl start mongod
+VALIDATE $? "mongodb service start state"
