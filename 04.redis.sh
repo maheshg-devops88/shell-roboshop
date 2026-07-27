dcf1183df3ff71b8698c059df77feb231e2c7ec1
mahesh <mahesh9.storage@gmail.com>
2026-07-27 12:04:34 +0530

    shell roboshop scripts

---
+++ 04.redis.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+userid=$(id -u)
+LOG_FOLDER=/var/log/shell-roboshop
+LOG_FILE=/var/log/shell-roboshop/$0.log
+WRK_DIR=$PWD
+
+if [ $userid -ne 0 ]; then
+    
+    echo "Please run the script sudo access"
+    exit 1
+fi 
+
+VALIDATE()
+if [ $1 == 0 ]; then
+   echo "$2.....Success" 
+ else
+   echo "$2.....Failure"
+fi
+
+mkdir -p $LOG_FOLDER
+VALIDATE $? "LOG directory creation "
+
+dnf module disable redis -y &>> $LOG_FILE
+VALIDATE $? "Disabled redis"
+dnf module enable redis:7 -y  &>> $LOG_FILE
+VALIDATE $? "Enabled redis 7"
+
+dnf install redis -y 
+VALIDATE $? "Install redis"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis/redis.conf
+
+sed -i 's/protected-mode yes/protected-mode no/g' /etc/redis/redis.conf
+
+systemctl enable redis 
+VALIDATE $? "Enable Systemctl service redis"
+systemctl start redis 
+VALIDATE $? "Start Systemctl service redis"
